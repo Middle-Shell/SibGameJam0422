67c5435c63704e167545610bbf41bf1991a2559c
Middle Shell <denob311302@gmail.com>
2022-05-01 12:31:11 +0300

    1

--- Assets/_Game/Scripts/PlayerController.cs
+++ Assets/_Game/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
+    private float distance = 10f;
+    [SerializeField]
     private bool _isButtonDown = false;
     private Vector3 _offset;
     private Rigidbody2D _rb;
@@ -18,29 +20,25 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        /*if (Input.GetMouseButtonDown(0))
-        {
-            _isButtonDown = true;
-                //_offset = gameObject.transform.position -
-                //          Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f));
-        }
+         if (Input.GetMouseButtonDown(0))
+         {
+             _isButtonDown = true;
+                 //_offset = gameObject.transform.position -
+                 //          Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f));
+         }
 
-        if (Input.GetMouseButtonUp(0))
-            _isButtonDown = false;
-        
-        if (_isButtonDown)
-        {
-            _rb.MovePosition(_rb.position + new Vector2(1f, 0f) * Time.deltaTime);
-            //Vector3 newPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f);
-            //transform.position = Camera.main.ScreenToWorldPoint(newPosition);
-        }*/
+         if (Input.GetMouseButtonUp(0))
+             _isButtonDown = false;
+         
+         if (_isButtonDown)
+         {
+             //_rb.MovePosition(_rb.position + new Vector2(1f, 0f) * Time.deltaTime);
+             Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance); 
+             Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition); 
+             transform.position = objPosition; 
+             //Vector3 newPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f);
+             //transform.position = Camera.main.ScreenToWorldPoint(newPosition);
+         }
     }
-    public float distance = 10f;
- 
-    void OnMouseDrag()
-    {
-        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance); // переменной записываються координаты мыши по иксу и игрику
-        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition); // переменной - объекту присваиваеться переменная с координатами мыши
-        transform.position = objPosition; // и собственно объекту записываються координаты
-    }
+    
 }
