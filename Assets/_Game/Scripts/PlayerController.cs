f077964e5e81dd22a2a980b9c95cdb8e82d63bbf
Middle Shell <denob311302@gmail.com>
2022-05-01 13:52:39 +0300

    Player

--- Assets/_Game/Scripts/PlayerController.cs
+++ Assets/_Game/Scripts/PlayerController.cs
@@ -8,23 +8,17 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float distance = 10f;
     [SerializeField]
+    private float _speed;
+    [SerializeField]
     private bool _isButtonDown = false;
     private Vector3 _offset;
-    private Rigidbody2D _rb;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        _rb = GetComponent<Rigidbody2D>();
-    }
+    private float _distanceToPoint;
 
     private void Update()
     {
          if (Input.GetMouseButtonDown(0))
          {
              _isButtonDown = true;
-                 //_offset = gameObject.transform.position -
-                 //          Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f));
          }
 
          if (Input.GetMouseButtonUp(0))
@@ -32,12 +26,11 @@ public class PlayerController : MonoBehaviour
          
          if (_isButtonDown)
          {
-             //_rb.MovePosition(_rb.position + new Vector2(1f, 0f) * Time.deltaTime);
              Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance); 
-             Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition); 
-             transform.position = objPosition; 
-             //Vector3 newPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f);
-             //transform.position = Camera.main.ScreenToWorldPoint(newPosition);
+             Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+             _distanceToPoint = Vector3.Distance(transform.position, objPosition);
+             Debug.Log(_distanceToPoint/_speed);
+             transform.position = Vector3.MoveTowards(transform.position, objPosition, _distanceToPoint/_speed);
          }
     }
     
