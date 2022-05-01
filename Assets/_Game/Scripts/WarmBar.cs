cfc92853a5d63bac6328e5b3469e19289eccae9e
D.vvan696 <D.vvaann@gmail.com>
2022-05-01 15:33:35 +0300

    save+bar

--- Assets/_Game/Scripts/WarmBar.cs
+++ Assets/_Game/Scripts/WarmBar.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WarmBar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Image bar;
+    public float fill = 1f;
+    public Gradient gradient;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        fill -= Time.deltaTime * 0.1f;
+        bar.fillAmount = fill;
     }
+
+    public void WormRestore()
+    {
+        fill += Time.deltaTime * 0.1f;
+    }
+
+
 }
