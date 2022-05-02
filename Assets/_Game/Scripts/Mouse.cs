49c36b6e409420c2d7bd573d15252380fdcc60d6
D.vvan696 <D.vvaann@gmail.com>
2022-05-02 16:09:52 +0300

    popitkaMouseMove

--- Assets/_Game/Scripts/Mouse.cs
+++ Assets/_Game/Scripts/Mouse.cs
@@ -2,12 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Mouse : MonoBehaviour
+{
 
-public class savePoint : MonoBehaviour
-{  
-    private PlayerController _player;
-     
-   
+    public float power  =5f;
+    // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("heeeeeey");
@@ -17,25 +16,29 @@ public class savePoint : MonoBehaviour
             // _player = GetComponent<PlayerController>();
 
             //InvokeRepeating("WarmHeal",1f,0.3f);
-           StartCoroutine(HealCoroutine(collision.GetComponent<PlayerController>()));
+           StartCoroutine(NoiceCoroutine(collision.GetComponent<NoiceMechanics>()));
             //Debug.Log("asdf");
             //collision.GetComponent<PlayerController>().WarmHeal();
         }
     }
-    IEnumerator HealCoroutine(PlayerController pl=null)
+
+        IEnumerator NoiceCoroutine(NoiceMechanics pl=null)
             {
                 while(true)
 	            {
-                pl.WarmHeal(); 
-                
-                yield return new WaitForSeconds(0.05f); 
+                pl.NoiceUp(power); 
+                Debug.Log("123");
+
+                yield return new WaitForSeconds(0.005f); 
                 
                 }
             }
-      private void OnTriggerExit2D(Collider2D collision)
+
+    private void OnTriggerExit2D(Collider2D collision)
       {
-         
+          
          StopAllCoroutines();
       }
-    
+
 }
+
